530da38d007e89c6998c2992a62ad1cdc667b7c8
Yew Wei <e1356261@u.nus.edu>
2025-08-16 23:48:12 +0800

    Briar.java: Add echo

--- src/main/java/Briar.java
+++ src/main/java/Briar.java
@@ -1,18 +1,34 @@
+import java.util.Scanner;
+
 public class Briar {
+    private Scanner scanner;
 
     private Briar() {
-
+        scanner = new Scanner(System.in);
     }
 
     public static void main(String[] args) {
         Briar briar = new Briar();
         briar.greet();
+        briar.listen();
         briar.exit();
     }
 
     private void greet() {
         System.out.println("Nice to meet you! I'm hungry... I mean Briar!");
         System.out.println("What can I do for you?");
+    }
+
+    private void listen() {
+        String command = "";
+        while (true) {
+            command = scanner.nextLine();
+            if (command.equals("bye")) {
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
     }
 
     private void exit() {
