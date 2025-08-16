bed4b63499ec945c0e39a763da813248f573357b
Yew Wei <e1356261@u.nus.edu>
2025-08-16 22:56:32 +0800

    Rename Duke.java to Briar.java

---
+++ src/main/java/Briar.java
@@ -0,0 +1,11 @@
+public class Briar {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm Briar");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
