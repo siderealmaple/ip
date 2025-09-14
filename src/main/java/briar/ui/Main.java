829f3ee10c0c2ffcddcb193cc0738b0c2b8cd764
siderealmaple <e1356261@u.nus.edu>
2025-09-14 12:13:03 +0800

    Add window icon

--- src/main/java/briar/ui/Main.java
+++ src/main/java/briar/ui/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -15,10 +16,14 @@ public class Main extends Application {
 
     private Briar briar = new Briar();
 
+    // Source: https://leagueoflegends.fandom.com/wiki/Briar/Development?file=I_WAS_ROOTING_FOR_YOU_Emote.png
+    private Image briarIcon = new Image(this.getClass().getResourceAsStream("/images/Briar.png"));
+
     @Override
     public void start(Stage stage) {
         try {
             stage.setTitle("Briar");
+            stage.getIcons().add(briarIcon);
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
